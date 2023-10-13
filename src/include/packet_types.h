60e5441ef3ae7c3f0fe7cfc24ebf5786b940c21e
linguini1 <matteo.golin@gmail.com>
2023-10-13 17:22:28 -0400

    Made unions packed tightly so they take only the amount of space written in the spec.

--- src/include/packet_types.h
+++ src/include/packet_types.h
@@ -53,27 +53,19 @@ typedef enum data_block_type {
 /* Allow any block sub-type from DataBlockType, CtrlBlockType or CmdBlockType. */
 typedef uint8_t BlockSubtype;
 
-/*
- * Each radio packet will have a header in this format. Any attribute labelled as dead space should be zero filled.
- *
- * callsign: Amateur radio call sign of the operator
- * length: The length of the packet in bytes
- * version: The version of the radio packet format being used
- * src_addr: The source address of the packet
- * packet_number: Which packet number the packet is in the stream being sent over radio
- * */
+/* Each radio packet will have a header in this format. Any attribute labelled as dead space should be zero filled. */
 typedef union packet_header {
     uint8_t bytes[12]; // Each packet header is 12 bytes
     uint32_t words[3]; // Each packet header is 3 32-bit words
     struct {
-        uint8_t callsign[6];
-        uint8_t length : 7;
-        uint8_t version : 5;
+        uint8_t callsign[6]; // Amateur radio call sign of the operator.
+        uint8_t length : 6;  // The length of the packet in bytes (including the header).
+        uint8_t version : 5; // The version of the radio packet format being used.
         uint8_t _dead_space : 5;
-        DeviceAddress src_addr : 4;
-        uint16_t packet_number : 12;
-        uint16_t _dead_space_2;
-    } contents;
+        DeviceAddress src_addr : 4;  // The source address of the packet.
+        uint16_t packet_number : 12; // Which packet number the packet is in the stream being sent over radio.
+        uint16_t _dead_space_2 : 16;
+    } __attribute__((packed, aligned(1))) contents;
 } PacketHeader;
 
 /* Casts the 8-bit ASCII call sign value to a null-terminated string. */
@@ -84,24 +76,18 @@ void packet_header_init(PacketHeader *p, const char *callsign, const uint8_t len
 
 /* Each block in the radio packet will have a header in this format. Any attribute labelled as dead space should be
  * zero-filled.
- *
- * length: The length of the block in bytes
- * has_sig: Whether or not the block has a cryptographic signature
- * type: The type of block
- * subtype: The block type's subtype
- * dest: The device address of the destination
  */
 typedef union block_header {
     uint8_t bytes[4];
     uint32_t words[1];
     struct {
-        uint8_t length : 5;
-        bool has_sig : 1;
-        BlockType type : 4;
-        BlockSubtype subtype : 6;
-        DeviceAddress dest : 4;
+        uint8_t length : 5;       // The length of the block in bytes.
+        bool has_sig : 1;         // Whether or not the block has a cryptographic signature.
+        BlockType type : 4;       // The type of block.
+        BlockSubtype subtype : 6; // The block type's subtype.
+        DeviceAddress dest : 4;   // The device address of the destination.
         uint16_t _dead_space : 12;
-    } contents;
+    } __attribute__((packed, aligned(1))) contents;
 } BlockHeader;
 
 void block_header_init(BlockHeader *b, const uint8_t length, const bool has_sig, const BlockType type,

6d7d04cf8bc21a1441135a275711037eb15c6bb0
linguini1 <matteo.golin@gmail.com>
2023-10-13 18:27:37 -0400

    Tried to set up argument parsing, but commented it for now since getopt doesn't seem to work yet.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include "packet_types.h"
 #include <stdint.h>
 #include <stdio.h>
-#include <getopt.h>
+#include <stdlib.h>
+
+static char *callsign = NULL;
 
 int main(int argc, char **argv) {
+
+    /* Fetch command line arguments. */
+    /* int c; */
+    /* while ((c = getopt(argc, argv, "c:")) != -1) { */
+    /*     switch (c) { */
+    /*     case 'c': */
+    /*         callsign = optarg; */
+    /*         break; */
+    /*     default: */
+    /*         fputs("Please see the 'use' page for usage.", stderr); */
+    /*         return EXIT_FAILURE; */
+    /*     } */
+    /* } */
 
     PacketHeader h;
     packet_header_init(&h, "VA3INI", 0, 0, ROCKET, 1);
 
     printf("Using callsign %s\n", packet_callsign(h));
-    return 0;
+    return EXIT_SUCCESS;
 }

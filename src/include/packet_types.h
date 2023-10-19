7c30dfd106da0a9e1864283d082b38d2285ce77f
Matthé Bekkers <matthedeb@hotmail.com>
2023-10-19 18:33:05 -0400

    Altitude-data-block

--- src/include/packet_types.h
+++ src/include/packet_types.h
@@ -147,4 +147,20 @@ typedef union signal_report_block {
 
 void signal_report_init(SignalReportBlock *b, const int8_t snr, const int8_t rssi, const uint8_t radio,
                         const int8_t tx_power, const bool request);
+
+typedef struct altitude_data_block{
+    /**The altitude data block accessed as a bytes array*/
+    uint8_t bytes[4];
+    /**The time at which the altitude was measured*/
+    int8_t measurment_type : 32;
+    /**The pressure at the measured atltitude*/
+    int8_t pressure : 32;
+    /**The temperature at the measured altitude*/
+    int8_t temperature : 32;
+    /**The measured altitude*/
+    int8_t altitude : 32;
+} TIGHTLY_PACKED AltitudeDataBlock; 
+
+void altitude_data_block_init(AltitudeDataBlock *b, const int8_t measurment_type, const uint8_t pressure, const uint8_t temperature,
+                              const uint8_t altitude);
 #endif // _PACKET_TYPES_H

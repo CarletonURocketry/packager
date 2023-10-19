25372f1d61b8df74ee10cd1e2f8d4b5ad48497bc
linguini1 <linguini1@users.noreply.github.com>
2023-10-19 13:55:33 +0000

    deploy: 07d5dda496e5f67af539c69ae66385230be7aa56

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['signal_5freport_5finit_0',['signal_report_init',['../packet__types_8c.html#a6bfae12fee48eb73f871f3443fa3f055',1,'signal_report_init(SignalReportBlock *b, const int8_t snr, const int8_t rssi, const uint8_t radio, const int8_t tx_power, const uint8_t request):&#160;packet_types.c'],['../packet__types_8h.html#a0b521ef6a3e32e6040a68a6a82d67ada',1,'signal_report_init(SignalReportBlock *b, const int8_t snr, const int8_t rssi, const uint8_t radio, const int8_t tx_power, const bool request):&#160;packet_types.h']]],
+  ['signal_5freport_5finit_0',['signal_report_init',['../packet__types_8c.html#a0b521ef6a3e32e6040a68a6a82d67ada',1,'signal_report_init(SignalReportBlock *b, const int8_t snr, const int8_t rssi, const uint8_t radio, const int8_t tx_power, const bool request):&#160;packet_types.c'],['../packet__types_8h.html#a0b521ef6a3e32e6040a68a6a82d67ada',1,'signal_report_init(SignalReportBlock *b, const int8_t snr, const int8_t rssi, const uint8_t radio, const int8_t tx_power, const bool request):&#160;packet_types.c']]],
   ['signalreportblock_1',['SignalReportBlock',['../unionSignalReportBlock.html',1,'']]],
   ['snr_2',['snr',['../unionSignalReportBlock.html#a1d04dad012bd061e199faa725058c5a3',1,'SignalReportBlock']]],
   ['src_5faddr_3',['src_addr',['../unionPacketHeader.html#a009d3532942da2c05e3a20db60e029f9',1,'PacketHeader']]],

ab2c2c5c9f8e092c890ee2c5686e7ad9c0128688
Matteo Golin <matteo.golin@gmail.com>
2023-11-02 12:30:53 -0400

    Packager can now read from stdin or a file.

--- src/main.c
+++ src/main.c
@@ -4,16 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 150
+
 static char *callsign = NULL;
+static char *file = NULL;
+static char buffer[BUFFER_SIZE] = {0};
 
 int main(int argc, char **argv) {
 
     /* Fetch command line arguments. */
     int c;
-    while ((c = getopt(argc, argv, ":c:")) != -1) {
+    while ((c = getopt(argc, argv, ":f:")) != -1) {
         switch (c) {
-        case 'c':
-            callsign = optarg;
+        case 'f':
+            file = optarg;
             break;
         case ':':
             fprintf(stderr, "Option -%c requires an argument.\n", optopt);
@@ -27,9 +31,26 @@ int main(int argc, char **argv) {
         }
     }
 
-    PacketHeader h;
-    packet_header_init(&h, "VA3INI", 0, 0, ROCKET, 1);
+    /* Check for positional arguments. */
+    if (optind >= argc) {
+        fputs("Call sign is required.", stderr);
+        exit(EXIT_FAILURE);
+    }
+    callsign = argv[optind];
+    printf("Using callsign %s\n", callsign);
 
-    printf("Using callsign %s\n", packet_callsign(h));
+    /* Open input stream. */
+    FILE *input;
+    if (file != NULL) {
+        input = fopen(file, "r");
+    } else {
+        input = stdin;
+    }
+
+    /* Read input data. */
+    while (fgets(buffer, BUFFER_SIZE, input) != NULL) {
+        printf("Packaged: %s", buffer);
+    }
+
     return EXIT_SUCCESS;
 }
